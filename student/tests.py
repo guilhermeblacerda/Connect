fb4a60483b4d3591fbef7100640303edaed8d32b
VitorCSA <vitorcarvalhosa12@gmail.com>
2025-10-20 12:21:18 -0300

    teste do absence iniciado

--- student/tests.py
+++ student/tests.py
@@ -7,6 +7,8 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from django.contrib.auth.models import User
 
 import time
@@ -23,7 +25,7 @@ class LoginE2ETeste(LiveServerTestCase):
         cls.browser.quit()
         super().tearDownClass()
 
-    def test_register_login(self):
+    def test_1_register_login(self):
 
         self.browser.get(f'{self.live_server_url}/register/') 
 
@@ -38,7 +40,9 @@ class LoginE2ETeste(LiveServerTestCase):
         password_input.send_keys('123456')
         password_input.send_keys(Keys.RETURN)
 
-        time.sleep(5)
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.presence_of_element_located((By.NAME,"login"))
+        )
 
         username_input = self.browser.find_element(By.NAME, 'username')
         password_input = self.browser.find_element(By.NAME, 'password')
@@ -49,9 +53,29 @@ class LoginE2ETeste(LiveServerTestCase):
 
         self.assertIn('Login',self.browser.page_source)
 
-        time.sleep(5)   
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.url_contains("home")
+        )
 
         self.assertIn('CONNECT',self.browser.page_source)
+
+    def test_2_absences(self):
+
+        self.browser.get(f'{self.live_server_url}/home/')
+
+        button = self.browser.find_element(By.TAG_NAME,"button")
+
+        button.click()
+
+        link = self.browser.find_element(By.NAME, "absence")
+
+        link.click()
+
+        webdriver(self.browser, 10).until(
+            expected_conditions.url_contains("absence")
+        ) 
+
+
       
 
 # Create your tests here.
